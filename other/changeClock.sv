0eaf0db53fc9c200fe2c315c2716180f3938cce4
Steven Li <99783345+oopstevenli@users.noreply.github.com>
2025-04-22 00:41:31 -0400

    Update changeClock.sv

--- other/changeClock.sv
+++ other/changeClock.sv
@@ -1,48 +1,61 @@
 `default_nettype none
-module changeClock(
-    input logic clock, reset, hour_increment, min_increment,
-    output logic [4:0] offset_hours, //0-23
-    output logic [5:0] offset_mins, //0-59
-    output logic [4:0] london_hours,
-    output logic [5:0] london_mins); 
-    logic prev_min, prev_hour, hour_edge, min_edge;
-    always_ff @(posedge clock) begin
-        prev_hour <= hour_increment;
-        prev_min <= min_increment;
-    end
-    assign hour_edge = hour_increment && !prev_hour;
-    assign min_edge = min_increment && !prev_min;
-   
-    always_ff @(posedge clock or posedge reset) begin
-        if (reset) begin
-            offset_hours <= 5'd0;
-            offset_mins <= 6'd0;
-        end
-        else begin 
-            if (min_edge) begin
-                offset_mins <= (offset_mins == 59) ? 6'd0 : offset_mins + 1;
-            end
-            if (hour_edge) begin
-                offset_hours <= (offset_hours == 23) ? 5'd0 : offset_hours + 1;
-            end
-        end
-    end
-    
-    always_comb begin
-        if (reset) begin
-            london_hours = offset_hours + 5;
-            london_mins = offset_mins;
-        end
-        if (min_edge) begin
-            london_mins = offset_mins;
-        end
-        if (hour_edge) begin
-            if (offset_hours < 19) begin //0 - 18(inclusive works)
-                london_hours = offset_hours + 5;
+module changeClock (
+	input  logic clock,
+	input  logic reset,
+	input  logic hour_increment,
+	input  logic min_increment,
+	output logic [4:0] offset_hours,
+	output logic [5:0] offset_mins,
+	output logic [4:0] london_hours,
+	output logic [5:0] london_mins
+	);
+
+	logic prev_min;
+	logic prev_hour;
+	logic hour_edge;
+	logic min_edge;
+	logic prev_reset;
+	logic reset_edge;
+	always_ff @(posedge clock) begin
+		prev_hour <= hour_increment;
+		prev_min <= min_increment;
+
+	end
+
+	always_ff @(posedge clock or posedge reset) begin
+		if (reset) begin
+			offset_hours <= 5'd0;
+			offset_mins <= 6'd0;
+			london_hours <= 5'd5;
+			london_mins <= 6'd0;
+		end
+		else begin
+			if (min_edge) begin
+				offset_mins <= (offset_mins == 6'd59 ? 6'd0 : offset_mins + 6'd1);
+				london_mins <= (london_mins == 6'd59 ? 6'd0 : london_mins + 6'd1);
             end
             else begin
-                london_hours = offset_hours - 19; //19 - 23 from original time
+                offset_mins <= 6'd0;
+                london_mins <= 6'd0;
             end
-        end
-    end
+			if (hour_edge) begin
+				offset_hours <= (offset_hours == 5'd23 ? 5'd0 : offset_hours + 5'd1);
+				//if (london_hours == 5'd0) begin
+				//    london_hours <= 5'd5;
+				//end
+				//else begin 
+				    london_hours <= (london_hours == 5'd23 ? 5'd0 : london_hours + 5'd1);
+                //end
+           end
+            else begin
+                offset_hours <= 5'd0;
+                london_hours <= 5'd0;
+            end
+		end 
+	end
+	
+	assign hour_edge = hour_increment && !prev_hour;
+	assign min_edge = min_increment && !prev_min;
+
+	
 endmodule
