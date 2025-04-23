346d815f14c9431ef3238cdb7f47452a3145fe2d
Steven Li <99783345+oopstevenli@users.noreply.github.com>
2025-04-23 16:34:51 -0400

    Update splitYear.sv

--- calendar/splitYear.sv
+++ calendar/splitYear.sv
@@ -2,8 +2,11 @@
 module splitYear(
     input logic [15:0] year_binary,
     output logic [6:0] leftNum, rightNum);
+    logic [15:0] temp_leftNum, temp_rightNum;
     always_comb begin
-        leftNum = (year_binary / 100);
-        rightNum = (year_binary % 100); //0-9
+        temp_leftNum = (year_binary / 100);
+        temp_rightNum = (year_binary % 100); //0-9
+        leftNum = temp_leftNum[6:0];
+        rightNum = temp_rightNum[6:0];
     end
 endmodule
