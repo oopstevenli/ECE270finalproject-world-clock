86087629533e5c8ca224cbdaa1722d6113390836
Steven Li <99783345+oopstevenli@users.noreply.github.com>
2025-04-24 15:00:11 -0400

    Update worldClock2.sv

--- other/worldClock2.sv
+++ other/worldClock2.sv
@@ -1,14 +1,14 @@
 module worldClock2 (
-	input  logic CLK12M,
-	input  logic [6:0] PB,
-	output logic [7:0] SS0,
-	output logic [7:0] SS1,
-	output logic [7:0] SS2,
-	output logic [7:0] SS3,
-	output logic [7:0] SS4,
-	output logic [7:0] SS5,
-	output logic [7:0] SS6,
-	output logic [7:0] SS7
+	input  logic hz100,
+	input  logic [6:0] pb,
+	output logic [7:0] ss0,
+	output logic [7:0] ss1,
+	output logic [7:0] ss2,
+	output logic [7:0] ss3,
+	output logic [7:0] ss4,
+	output logic [7:0] ss5,
+	output logic [7:0] ss6,
+	output logic [7:0] ss7
 	);
 
 	logic [6:0] SYNTHESIZED_WIRE_0;
@@ -30,7 +30,7 @@ module worldClock2 (
 	logic [5:0] SYNTHESIZED_WIRE_17;
 	logic SYNTHESIZED_WIRE_18;
 	muxModule b2v_inst0(
-		.select(PB[1]),
+		.select(pb[1]),
 		.day(SYNTHESIZED_WIRE_0),
 		.hour_1(SYNTHESIZED_WIRE_1),
 		.hour_2(SYNTHESIZED_WIRE_2),
@@ -46,27 +46,27 @@ module worldClock2 (
 	);
 	digit2Display b2v_inst1(
 		.value(SYNTHESIZED_WIRE_8),
-		.left(SS7),
-		.right(SS6)
+		.left(ss7),
+		.right(ss6)
 	);
 	digit2Display b2v_inst2(
 		.value(SYNTHESIZED_WIRE_9),
-		.left(SS5),
-		.right(SS4)
+		.left(ss5),
+		.right(ss4)
 	);
 	digit2Display b2v_inst3(
 		.value(SYNTHESIZED_WIRE_10),
-		.left(SS3),
-		.right(SS2)
+		.left(ss3),
+		.right(ss2)
 	);
 	digit2Display b2v_inst4(
 		.value(SYNTHESIZED_WIRE_11),
-		.left(SS1),
-		.right(SS0)
+		.left(ss1),
+		.right(ss0)
 	);
 	clock24 b2v_inst5(
 		.clk(SYNTHESIZED_WIRE_21),
-		.reset(PB[0]),
+		.reset(pb[0]),
 		.offset_hours(SYNTHESIZED_WIRE_13),
 		.offset_mins(SYNTHESIZED_WIRE_14),
 		.cycle(SYNTHESIZED_WIRE_18),
@@ -75,7 +75,7 @@ module worldClock2 (
 	);
 	clock24 b2v_inst6(
 		.clk(SYNTHESIZED_WIRE_21),
-		.reset(PB[0]),
+		.reset(pb[0]),
 		.offset_hours(SYNTHESIZED_WIRE_16),
 		.offset_mins(SYNTHESIZED_WIRE_17),
 		.hours(SYNTHESIZED_WIRE_2),
@@ -84,10 +84,10 @@ module worldClock2 (
 	calendar b2v_inst7(
 		.hour_enable(SYNTHESIZED_WIRE_18),
 		.clock(SYNTHESIZED_WIRE_21),
-		.reset(PB[0]),
-		.day_increment(PB[2]),
-		.year_increment(PB[3]),
-		.month_increment(PB[4]),
+		.reset(pb[0]),
+		.day_increment(pb[2]),
+		.year_increment(pb[3]),
+		.month_increment(pb[4]),
 		.day_binary(SYNTHESIZED_WIRE_0),
 		.LeftYear(SYNTHESIZED_WIRE_6),
 		.month_binary(SYNTHESIZED_WIRE_5),
@@ -95,17 +95,17 @@ module worldClock2 (
 	);
 	changeClock b2v_inst8(
 		.clock(SYNTHESIZED_WIRE_21),
-		.reset(PB[0]),
-		.hour_increment(PB[5]),
-		.min_increment(PB[6]),
+		.reset(pb[0]),
+		.hour_increment(pb[5]),
+		.min_increment(pb[6]),
 		.london_hours(SYNTHESIZED_WIRE_16),
 		.london_mins(SYNTHESIZED_WIRE_17),
 		.offset_hours(SYNTHESIZED_WIRE_13),
 		.offset_mins(SYNTHESIZED_WIRE_14)
 	);
-	clk_div_1hz b2v_inst9(
-		.clk12m(CLK12M),
-		.reset(PB[0]),
+	clk100HzTo1Hz b2v_inst9(
+		.clk100hz(hz100),
+		.reset(pb[0]),
 		.clk1hz(SYNTHESIZED_WIRE_21)
 	);
 	defparam b2v_inst9.DIVISOR = 12000000;
