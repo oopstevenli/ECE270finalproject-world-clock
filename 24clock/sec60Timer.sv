3f47403e5e9b4e28bad6a058ea231de889b5e79f
Steven Li <99783345+oopstevenli@users.noreply.github.com>
2025-04-24 14:52:56 -0400

    Update sec60Timer.sv

--- 24clock/sec60Timer.sv
+++ 24clock/sec60Timer.sv
@@ -2,19 +2,19 @@
 module sec60Timer(
     input logic clk, reset,
     output logic cycle,
-    output logic [5:0] seconds
+    output logic [6:0] seconds
 );
-logic [5:0] Q, nextQ;
+logic [6:0] Q, nextQ;
 always_ff @(posedge clk or posedge reset) begin
     if(reset) 
-        Q <= 6'd0;
+        Q <= 7'd0;
     else
         Q <= nextQ;
 end
 always_comb begin
-    if(Q == 6'd59) begin
+    if(Q == 7'd59) begin
         cycle = 1'b1;
-        nextQ = 6'd0;
+        nextQ = 7'd0;
     end
     else begin
         nextQ = Q + 1;
