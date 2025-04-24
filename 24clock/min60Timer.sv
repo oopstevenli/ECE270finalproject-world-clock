7095f2195d818347e18ec0cfd22fff336d482bfd
Steven Li <99783345+oopstevenli@users.noreply.github.com>
2025-04-24 14:52:28 -0400

    Update min60Timer.sv

--- 24clock/min60Timer.sv
+++ 24clock/min60Timer.sv
@@ -3,19 +3,19 @@ module min60Timer(
     input logic clk, en, reset,
     input logic [5:0] offset,
     output logic cycle,
-    output logic [5:0] mins
+    output logic [6:0] mins
 );
-logic [5:0] Q, nextQ;
+logic [6:0] Q, nextQ;
 always_ff @(posedge clk or posedge reset) begin
     if (reset) 
-        Q <= 6'd0;
+        Q <= 7'd0;
     else
         Q <= nextQ;
 end
 always_comb begin
-    nextQ = Q + offset + en;
-    if(nextQ > 6'd59) begin
-        nextQ = 6'd0;
+    nextQ = Q + {1'b0, offset} + {6'b0, en};
+    if(nextQ > 7'd59) begin
+        nextQ = 7'd0;
         cycle = 1'b1;
     end
     else begin
