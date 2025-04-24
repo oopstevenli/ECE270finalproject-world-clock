667a483f0e124d8110df6470fcba7be99a0659f1
Steven Li <99783345+oopstevenli@users.noreply.github.com>
2025-04-24 14:52:08 -0400

    Update day24Timer.sv

--- 24clock/day24Timer.sv
+++ 24clock/day24Timer.sv
@@ -3,19 +3,19 @@ module day24Timer(
     input logic clk, en, reset,
     input logic [4:0] offset,
     output logic cycle,
-    output logic [4:0] hours
+    output logic [6:0] hours
 );
-logic [5:0] Q, nextQ;
+logic [6:0] Q, nextQ;
 always_ff @(posedge clk or posedge reset) begin
     if (reset) 
-        Q <= 5'd0;
+        Q <= 7'd0;
     else
         Q <= nextQ;
 end
 always_comb begin
-    nextQ = Q + offset + en;
-    if(nextQ > 5'd24) begin
-        nextQ = 5'd0;
+    nextQ = Q + {2'b0, offset} + {6'b0, en};
+    if(nextQ > 7'd24) begin
+        nextQ = 7'd0;
         cycle = 1'b1;
     end
     else begin
